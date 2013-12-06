c2a163e65588a187ea54718950ae9f0ae345d3f8
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-06 14:05:22 -0700

    add fixtures

--- js/app.js
+++ js/app.js
@@ -1,11 +1,35 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+App.Song = Ember.Object.extend({
+  title: null,
+  rating: null,
+  artist: null
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+App.Songs = Ember.A();
+
+var blackDog = Ember.Song.create({
+     title: "Black Dog",
+    artist: "Led Zepplin",
+    rating: 8
+});
+var yellowLedbetter = Ember.Song.create({
+     title: "Yellow Ledbetter",
+    artist: "Pearl Jam",
+    rating: 10
+});
+var thePretender = Ember.Song.create({
+     title: "The Pretender",
+    artist: "Foo Fighters",
+    rating: 10
+});
+
+App.Songs.pushObject(blackdog);
+App.Songs.pushObject(yellowDedbetter);
+App.Songs.pushObject(thePretender);
+
+App.alwaysWaiting = App.Song.create({
+  title: "Always Waiting",
+  artist: "Kaya Project",
+  rating: 9
 });
