ca6cc238779dcd1b211debdbb8643761d48cf4c0
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-07 10:25:45 -0700

    supposed to be outputting songs... but broken

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,9 @@
 App = Ember.Application.create();
 
 App.Router.map(function(){
-  this.route('artists');
+  this.resource('artists', function() {
+    this.route('songs', { path: ':slug' });
+  });
 });
 
 App.ArtistsRoute = Ember.Route.extend({
@@ -10,8 +12,22 @@ App.ArtistsRoute = Ember.Route.extend({
   }
 });
 
+App.ArtistsSongRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.Artists.findBy('slug', params.slug);
+  }
+});
+
 App.Artist = Ember.Object.extend({
-  name: null
+  name: null,
+
+  slug: function() {
+    return this.get('name').dasherize();
+  }.property('name'),
+
+  songs: function() {
+    return App.Songs.filterProperty('artist', this.get('name'));
+  }.property('name', 'App.Songs.@each.artist')
 });
 
 App.Song = Ember.Object.extend({
@@ -40,15 +56,15 @@ App.Songs.pushObject(App.Song.create({title: 'Alive', artist: 'Pearl Jam', ratin
 App.Songs.pushObject(App.Song.create({title: 'Given to Fly', artist: 'Pearl Jam', rating: 7}));
 App.Songs.pushObject(App.Song.create({title: 'Inside Job', artist: 'Pearl Jam', rating: 9}));
 
-// Led Zeppeling songs
-App.Songs.pushObject(App.Song.create({title: 'Black Dog', artist: 'Pearl Jam', rating: 8}));
-App.Songs.pushObject(App.Song.create({title: 'Achilles Last Stand', artist: 'Pearl Jam', rating: 10}));
-App.Songs.pushObject(App.Song.create({title: 'Immigrant Song', artist: 'Pearl Jam', rating: 7}));
-App.Songs.pushObject(App.Song.create({title: 'Whole Lotta Love', artist: 'Pearl Jam', rating: 7}));
+// Led Zeppelin songs
+App.Songs.pushObject(App.Song.create({title: 'Black Dog', artist: 'Led Zeppelin', rating: 8}));
+App.Songs.pushObject(App.Song.create({title: 'Achilles Last Stand', artist: 'Led Zeppelin', rating: 10}));
+App.Songs.pushObject(App.Song.create({title: 'Immigrant Song', artist: 'Led Zeppelin', rating: 7}));
+App.Songs.pushObject(App.Song.create({title: 'Whole Lotta Love', artist: 'Led Zeppelin', rating: 7}));
 
 // Foo Fighters songs
-App.Songs.pushObject(App.Song.create({title: 'The Pretender', artist: 'Pearl Jam', rating: 6}));
-App.Songs.pushObject(App.Song.create({title: 'Best of You', artist: 'Pearl Jam', rating: 10}));
+App.Songs.pushObject(App.Song.create({title: 'The Pretender', artist: 'Foo Fighters', rating: 6}));
+App.Songs.pushObject(App.Song.create({title: 'Best of You', artist: 'Foo Fighters', rating: 10}));
 
 // Kaya Project songs
 App.Songs.pushObject(App.Song.create({title: "Always Waiting", artist: "Kaya Project", rating: 9}));
